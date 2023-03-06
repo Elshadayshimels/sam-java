2ad61754b220ab165892d33b1e0708aff59f2466
elshadayshimels <1elshaday2@gmail.com>
2023-03-06 13:29:39 -0800

    java test 1

---
+++ sam/src/sam/sam_java.java
@@ -0,0 +1,21 @@
+package comparing;
+
+public class tt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 20;
+	      int num2 = 35;
+	   
+	      if(num1 == num2) {
+	         System.out.println("The two numbers are equal.");
+	      }
+	      else if(num1 > num2) {
+	         System.out.println("Number 1 is greater than number 2.");
+	      }
+	      else {
+	         System.out.println("Number 2 is greater than number 1.");
+
+	}
+
+	}}
